86fc04dab9e74a77247a0fa19198990ea960fc5f
MosheM123 <158142047+MosheM123@users.noreply.github.com>
2024-02-04 12:33:16 +0200

    Update settings.py

--- fitt/app/settings.py
+++ fitt/app/settings.py
@@ -19,16 +19,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-m=f=!a#hhs#(rz2@6i-k%nk6ab^q_(6#tx#u2y&um$&@ah+q-)')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '1f02565ad9bb3bae5acea8e48d9ba9fec2a4a9d40d247218526f21d37303d04d32056d96d033f18c306d46b5fbe4cedf4f4450')
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = 'django-insecure-m=f=!a#hhs#(rz2@6i-k%nk6ab^q_(6#tx#u2y&um$&@ah+q-)'
+SECURE_HSTS_PRELOAD = True
+SECURE_HSTS_SECONDS = 518400
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app']
-
-
+ALLOWED_HOSTS = ['']
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
 # Application definition
 
 INSTALLED_APPS = [
