cafe81144fb94a5f701d83321a557162d6e36604
MosheM123 <158142047+MosheM123@users.noreply.github.com>
2024-02-04 12:22:38 +0200

    Update settings.py

--- fitt/app/settings.py
+++ fitt/app/settings.py
@@ -19,9 +19,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-m=f=!a#hhs#(rz2@6i-k%nk6ab^q_(6#tx#u2y&um$&@ah+q-)')
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m=f=!a#hhs#(rz2@6i-k%nk6ab^q_(6#tx#u2y&um$&@ah+q-)'
+#SECRET_KEY = 'django-insecure-m=f=!a#hhs#(rz2@6i-k%nk6ab^q_(6#tx#u2y&um$&@ah+q-)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
