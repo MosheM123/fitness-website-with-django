d9942311c8918c8e0aa397e7bdfd6984dad24b86
MosheM123 <158142047+MosheM123@users.noreply.github.com>
2024-02-06 09:11:10 +0200

    Update settings.py

--- fitt/app/settings.py
+++ fitt/app/settings.py
@@ -19,24 +19,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '1f02565ad9bb3bae5acea8e48d9ba9fec2a4a9d40d247218526f21d37303d04d32056d96d033f18c306d46b5fbe4cedf4f4450')
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-m=f=!a#hhs#(rz2@6i-k%nk6ab^q_(6#tx#u2y&um$&@ah+q-)'
-SECURE_HSTS_PRELOAD = True
-SECURE_HSTS_SECONDS = 518400
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECRET_KEY = 'django-insecure-m=f=!a#hhs#(rz2@6i-k%nk6ab^q_(6#tx#u2y&um$&@ah+q-)'
+
 
 DEBUG = False
 
 
-ALLOWED_HOSTS = ['']
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-if RENDER_EXTERNAL_HOSTNAME:
-    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
 
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
-SECURE_SSL_REDIRECT = True
 # Application definition
 
 INSTALLED_APPS = [
